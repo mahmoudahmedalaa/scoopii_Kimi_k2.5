805429f79c73299052c08a70c61a2a77a14177cd
mahmoudahmedalaa <mahmoudahmedalaa@gmail.com>
2026-02-02 16:45:16 +0400

    fix(app): restore App.tsx content

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-
+import { LandingPage } from '@/pages/LandingPage';
+import { Admin } from '@/pages/Admin';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-import { LandingPage } from '@/pages/LandingPage';
-  import { Admin } from '@/pages/Admin';
-  import { BrowserRouter, Routes, Route } from 'react-router-dom';
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-  function App() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/admin" element={<Admin />} />
-        </Routes>
-      </BrowserRouter>
-    );
-    export default App;
+export default App;
